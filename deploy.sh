62b5e8e69dab81f561138fadcaedf11abb0bdb62
Darren Oakley <daz.oakley@gmail.com>
2016-07-23 09:05:50 +0100

    Update deployment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-bin/html_table > medal-table.html
+bin/html_table > medal-table/index.html
 
-scp medal-table.html admin@londonamateurbrewers.co.uk:/srv/londonamateurbrewers.co.uk/public/htdocs/medal-table.html
+scp -r medal-table admin@londonamateurbrewers.co.uk:/srv/londonamateurbrewers.co.uk/public/htdocs/medal-table
 
